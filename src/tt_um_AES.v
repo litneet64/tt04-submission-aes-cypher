8265da84dc94f36135a15fd92a2cf2cc75a1a41e
litneet64 <blabla@uwu.com>
2023-08-22 17:17:56 -0400

    fix const syntax

--- src/tt_um_AES.v
+++ src/tt_um_AES.v
@@ -8,10 +8,10 @@ module tt_um_AES(input wire ena,
             output wire [7:0] uio_oe
             );
 
-wire[127:0] in;
+wire [127:0] in;
 
 // The plain text used as input
-assign in = {120'h_00112233445566778899aabbccddee', ui_in[7:0]};
+assign in = {120'h_00112233445566778899aabbccddee, ui_in[7:0]};
 
 // The different keys used for testing (one of each type)
 wire[127:0] key128 = 128'h_000102030405060708090a0b0c0d0e0f;
