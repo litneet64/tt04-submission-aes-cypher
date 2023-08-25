0a3645ba440e61e7df2bccc5c06e7ce50cb4e967
litneet64 <blabla@uwu.com>
2023-08-24 21:01:36 -0400

    add use 8-bit input

--- src/tt_um_AES.v
+++ src/tt_um_AES.v
@@ -1,14 +1,18 @@
-module tt_um_AES(input wire ena,
-            input wire clk,
-            input wire rst_n,
-            input wire [7:0] ui_in,
-            input wire [7:0] uio_in,
-            output wire [7:0] uo_out,
-            output wire [7:0] uio_out,
-            output wire [7:0] uio_oe
-            );
+module tt_um_AES(
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
-wire [127:0] in = 128'h00112233445566778899aabbccddeeff;
+wire [127:0] in;
+wire [119:0] fixed_in = 120'h00112233445566778899aabbccddee;
+
+in = {fixed_in, ui_in};
 
 // The plain text used as input
 //wire[119:0] fixed_in = 120'h_00112233445566778899aabbccddee;
